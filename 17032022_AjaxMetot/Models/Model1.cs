554423270ab5188787e2f0ba454561c0ce773201
Meryemnursikak <meryemnursikak@gmail.com>
2022-03-22 10:17:44 +0300

    Add project files.

---
+++ 17032022_AjaxMetot/Models/Model1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _17032022_AjaxMetot.Models
+{
+    public class Model1
+    {
+        public Guid ID { get; set; }
+        public string Text { get; set; }
+    }
+}
